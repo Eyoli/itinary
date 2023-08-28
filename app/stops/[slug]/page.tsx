b39383e1fde2dd852c7ede0891103366e0e7bc74
clement_obert <clement_obert@ext.connect-tech.sncf>
2023-08-28 16:57:58 +0200

    rework app to make it static

--- app/stops/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllStops } from "@/src/services/gtfs";
-import { Stop } from "@/src/components/Stop";
-
-export default async function Page({ params }: { params: { slug: string } }) {
-  const data = await getAllStops();
-  const stop = data.find((s) => s.stop_id === decodeURIComponent(params.slug));
-
-  return stop && <Stop stop={stop} />;
-}
--- app/trips/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllTrips } from "@/src/services/gtfs";
-import { Trip } from "@/src/components/Trip";
-
-export default async function Page({ params }: { params: { slug: string } }) {
-  const data = await getAllTrips();
-  const trip = data.find((t) => t.trip_id === decodeURIComponent(params.slug));
-
-  return <main className="px-2">{trip && <Trip trip={trip} />}</main>;
-}
